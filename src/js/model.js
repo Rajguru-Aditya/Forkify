7086ecf719256edae7d03bb14968bfe07c2a74df
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-06-19 14:32:53 +0530

    Another commit

--- src/js/model.js
+++ src/js/model.js
@@ -124,12 +124,9 @@ export const uploadRecipe = async function(newRecipe){
         const ingredients = Object.entries(newRecipe)
             .filter(entry => entry[0].startsWith("ingredient") && entry[1] !== "")
             .map(ing => {
-                // const ingArr = ing[1]
-                //     .replaceAll(" ", "")
-                //     .split(",");
                 const ingArr = ing[1]
-                    .split(",")
-                    .map(el => el.trim);
+                    .replaceAll(" ", "")
+                    .split(",");
                 if(ingArr.length !== 3) throw new Error("Wrong ingredient format! Please use the correct format :)")
                 const [quantity, unit, description] = ingArr;
                 return { quantity: quantity ? +quantity : null, unit, description };
